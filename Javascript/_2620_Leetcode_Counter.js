7c64f69d77e352b2cbfd2cdb633f160827cec287
DinhVanNhan <vannhan10102004@gmail.com>
2025-07-06 13:40:46 +0700

    Leetcode: 2620. Counter.

---
+++ Javascript/_2620_Leetcode_Counter.js
@@ -0,0 +1,13 @@
+function createCounter(n: number): () => number {
+  let num = n;
+  return function () {
+    return num++;
+  };
+}
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
