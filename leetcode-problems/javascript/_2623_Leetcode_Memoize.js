4c3ec7adfafdfffa22928556baa59a9f2f4ef019
nhandinhvan <vannhan2409@gmail.com>
2026-06-07 22:17:13 +0700

    Leetcode: 2623. Memoize

---
+++ leetcode-problems/javascript/_2623_Leetcode_Memoize.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+  // let memo = new Map()
+  let memo = {};
+
+  return function (...args) {
+    const key = args.join(",");
+
+    if (key in memo) {
+      return memo[key];
+    }
+
+    const result = fn(...args);
+
+    memo[key] = result;
+
+    return result;
+  };
+}
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
