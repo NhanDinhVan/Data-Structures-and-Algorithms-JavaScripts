9d1dc0c88946b06cda9e6becbf4cdfd47110af6f
nhandinhvan <vannhan2409@gmail.com>
2026-06-07 21:42:21 +0700

    Leetcode: 2629. Function Composition

---
+++ leetcode-problems/javascript/_2629_Leetcode_Function_Composition.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+  // return function(x) {
+  //     let result = x
+
+  //     for(let i = functions.length -1; i >= 0; i--) {
+  //         result = functions[i](result)
+  //     }
+
+  //     return result
+  // }
+
+  return function (x) {
+    return functions.reduceRight((acc, func) => {
+      return func(acc);
+    }, x);
+  };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
